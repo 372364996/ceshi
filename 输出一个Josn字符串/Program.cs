020155c81fa998777570017dfcc9d28ac0366a80
Mr-Tommy <372364996@qq.com>
2017-02-06 17:35:41 +0800

    测试输出json字符串

---
+++ 输出一个Josn字符串/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 输出一个Josn字符串
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string msg = string.Format(@"{'IsDing':false,'IsInput':false,'Id':null,'Type':2,'From':{'Id':{0},'Name':'Kenny.Lee','Type':0},'To':'','DingCount':0,'HeadImg':'https://stream-cdn.weikebang.com/headimgs/40405','sdaf':'qe qwe','Top':false,'Data':'老郑不要玻璃心 哈哈','Reply':null,'IsReply':false}",1);
+         
+            Console.WriteLine(msg);
+            Console.ReadKey();
+        }
+    }
+}
